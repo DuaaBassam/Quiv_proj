03beda4165107be2163511a223e57092f11b29fc
Hanan Dawod <hanandawod20@gmail.com>
2017-06-16 23:53:09 +0200

    Merge branch 'master' of C:\Users\Hanan Dawod\.android\Quiv_proj with conflicts.

--- app/src/main/java/net/androidbootcamp/quiv_proj/NewFragment.java
+++ app/src/main/java/net/androidbootcamp/quiv_proj/NewFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import static net.androidbootcamp.quiv_proj.R.layout.new_frag;
 
@@ -14,6 +16,26 @@ public class NewFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.new_frag,container,false);
+        View view = inflater.inflate(R.layout.new_frag, container, false);
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something
+                //
+                //
+                } });
+        return view;
+    }
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new
+            ArrayAdapter <String>(getActivity(), android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter); }
+
+    public void setListAdapter(ArrayAdapter<String> listAdapter) {
+        listAdapter = listAdapter;
     }
 }
