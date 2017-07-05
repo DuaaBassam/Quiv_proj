6a986be91a6c25c5860518f9701f69e72d2fa89f
Hanan Dawod <hanandawod20@gmail.com>
2017-07-05 12:28:15 +0200

    fragment

--- app/src/main/java/net/androidbootcamp/quiv_proj/AddStudent.java
+++ app/src/main/java/net/androidbootcamp/quiv_proj/AddStudent.java
@@ -16,29 +16,9 @@ import android.widget.TextView;
  */
 
 public class AddStudent extends Fragment{
-        // Store instance variables
-        private String title;
-        //private int page;
+
 DatabaseHelper db;
-        // newInstance constructor for creating fragment with arguments
-        public static AddStudent newInstance(int page, String title) {
-            AddStudent fragmentFirst = new AddStudent();
-            Bundle args = new Bundle();
-            args.putInt("someInt", page);
-            // args.putString("someTitle", title);
-            fragmentFirst.setArguments(args);
-            return fragmentFirst;
-        }
 
-        // Store instance variables based on arguments passed
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            //page = getArguments().getInt("someInt", 0);
-            title = getArguments().getString("someTitle");
-        }
-
-        // Inflate the view for the fragment based on layout XML
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
--- app/src/main/java/net/androidbootcamp/quiv_proj/StudentInTeacher.java
+++ app/src/main/java/net/androidbootcamp/quiv_proj/StudentInTeacher.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by مركز الخبراء on 07/04/2017.
@@ -23,95 +25,38 @@ public class StudentInTeacher extends Fragment {
                              Bundle savedInstanceState) {
 
 
-        Log.d("name","      " + getArguments().getString("nameCourse"));
+        Log.d("name", "      " + getArguments().getString("nameCourse"));
 
 
-        View view =inflater.inflate(R.layout.student_in_teacher, container, false);
-        ViewPager vpPager = (ViewPager) view.findViewById(R.id.vpPager);
-        adapterViewPager = new MyPagerAdapter(getActivity().getSupportFragmentManager());
-        vpPager.setAdapter(adapterViewPager);
-// Attach the page change listener inside the activity
-        vpPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        View view = inflater.inflate(R.layout.student_in_teacher, container, false);
+        Button addd = (Button)view.findViewById(R.id.addd);
+        Button showw = (Button)view.findViewById(R.id.showw);
 
-            // This method will be invoked when a new page becomes selected.
+        addd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPageSelected(int position) {
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.add_delete_show, new AddStudent());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
 
-//                PagerTabStrip mPagerTabStrip = (PagerTabStrip) findViewById(R.id.pager_header);
-//                for (int i = 0; i < mPagerTabStrip.getChildCount(); ++i) {
-//                    View nextChild = mPagerTabStrip.getChildAt(i);
-//                    TextView textViewToConvert;
-//                    if (nextChild instanceof TextView) {
-//                        textViewToConvert = (TextView) nextChild;
-//                        //textViewToConvert.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                        textViewToConvert.setTextColor(Color.RED);
-//                    }else
-//                        textViewToConvert = (TextView) nextChild;
-//                    //textViewToConvert.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-//                    textViewToConvert.setTextColor(Color.BLUE);
-                //  }
-            }
+                }
 
-
-            // This method will be invoked when the current page is scrolled
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                // Code goes here
-            }
-
-            // Called when the scroll state changes:
-            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                // Code goes here
-            }
         });
-        return view;
-    }
 
-    public  class MyPagerAdapter extends FragmentPagerAdapter {
-        private  int NUM_ITEMS = 2;
+        showw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.add_delete_show, new ShowStudent());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
 
-        public MyPagerAdapter(FragmentManager fragmentManager) {
-            super(fragmentManager);
-        }
-
-        // Returns total number of pages
-        @Override
-        public int getCount() {
-            return NUM_ITEMS;
-        }
-
-        // Returns the fragment to display for that page
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0: // Fragment # 0 - This will show FirstFragment
-                    Fragment fragment =new AddStudent();
-                    Bundle bundle=new Bundle();
-                    bundle.putString("name",getArguments().getString("nameCourse"));
-                    fragment.setArguments(bundle);
-
-                    return  AddStudent.newInstance(0, "Page # 1");
-
-                case 1: // Fragment # 0 - This will show FirstFragment different title
-                    return ShowStudent.newInstance(1, "Page # 2");
-
-                default:
-                    return null;
             }
-        }
 
-        // Returns the page title for the top indicator
-        @Override
-        public CharSequence getPageTitle(int position) {
-            if(position==0){
-                return "ADD / DELETE" ;}
-            return "SHOW";
-        }
+        });
 
-
-    }
-
-}
-
+        return view ;
+    }}
