0ff5e30455ebe64448268209dfa0baabdb2f7986
Hanan Dawod <hanandawod20@gmail.com>
2017-07-24 14:53:18 +0200

    quiz

--- app/src/main/java/net/androidbootcamp/quiv_proj/AddQuiz.java
+++ app/src/main/java/net/androidbootcamp/quiv_proj/AddQuiz.java
@@ -1,5 +1,8 @@
 package net.androidbootcamp.quiv_proj;
 
+
+import android.app.Dialog;
+import android.preference.CheckBoxPreference;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -9,37 +12,152 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
 
-public class AddQuiz extends Fragment implements FragmentLifecycle{
+
+public class AddQuiz extends Fragment implements FragmentLifecycle {
 
     DatabaseHelper db;
     private String nameCourse;
     private int idTeach;
-    private String teacher;
+    private String answer;
+    int indexAns = 0;
+    int indexCorrect = 0;
+
+    HashMap answerQuestion =new HashMap();
+    HashMap correctAnswer =new HashMap();
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        nameCourse = getArguments().getString("namee");
-  //      idTeach = getArguments().getInt("idTeach");
-
+        nameCourse = getArguments().getString("namee");
+        idTeach = getArguments().getInt("idTeach");
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.add_quiz, container, false);
-
-
-    //    Log.d("nameCC", "      " + nameCourse+"   "+idTeach);
-
-
+        Button Add = (Button) view.findViewById(R.id.addquiz);
+        Button Submit = (Button) view.findViewById(R.id.submit);
+        final Button addAnswer = (Button) view.findViewById(R.id.addAnswer);
+        final EditText nameQuiz = (EditText) view.findViewById(R.id.nameQuiz);
+        final EditText password = (EditText) view.findViewById(R.id.password);
+        TextView noQuiz = (TextView) view.findViewById(R.id.quesNo);
+        final EditText question = (EditText) view.findViewById(R.id.question);
+        final EditText answer = (EditText) view.findViewById(R.id.answer);
+        final RadioButton radioButton1 = (RadioButton) view.findViewById(R.id.radioButton1);
+        final RadioButton radioButton2 = (RadioButton) view.findViewById(R.id.radioButton2);
+        final CheckBox correct = (CheckBox) view.findViewById(R.id.checkBox2);
         db = new DatabaseHelper(getActivity());
+
+        addAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String answerQues = answer.getText().toString();
+                if (!(question.getText().toString()).isEmpty()) {
+                    if (!answerQues.isEmpty()) {
+                        answerQuestion.put(indexAns,answerQues);
+                        indexAns++;
+                        answer.setText("");
+                        if (correct.isChecked()) {
+                            correctAnswer.put(indexCorrect,answerQues);
+                            indexCorrect++;
+                            correct.toggle();
+
+                        }
+                    } else {
+                        Toast.makeText(getActivity(), "Write answer.", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } else {
+                    Toast.makeText(getActivity(), "Write question.", Toast.LENGTH_SHORT).show();
+
+                }
+            }});
+        final Dialog dialog= new Dialog(getActivity());
+
+
+
+    Submit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if((!(nameQuiz.getText().toString()).isEmpty())&& (!(password.getText().toString()).isEmpty())){
+              if(radioButton1.isChecked()){
+                  dialog.setContentView(R.layout.submit);
+                  dialog.setTitle("Question");
+                  Button btn_dialog = (Button)dialog.findViewById(R.id.cancel_submit);
+                  btn_dialog.setOnClickListener(new View.OnClickListener() {
+                      @Override
+                      public void onClick(View view) {
+                          dialog.cancel();
+                      }
+                  });
+                  Button btn_ok = (Button)dialog.findViewById(R.id.ok_submit);
+
+                  btn_ok.setOnClickListener(new View.OnClickListener() {
+                      @Override
+
+                      public void onClick(View view) {
+                        db.insertDataQuiz(nameQuiz.getText().toString(),Integer.parseInt(password.getText().toString()),
+                                idTeach,0);
+                          Toast.makeText(getActivity(), "Add Quiz ", Toast.LENGTH_SHORT).show();
+                           dialog.cancel();
+                          nameQuiz.setText("");
+                          password.setText("");
+                      }
+                  });
+                  dialog.show();
+              }
+                else if(radioButton2.isChecked()){
+                  dialog.setContentView(R.layout.dialog);
+                  dialog.setTitle("Question");
+                  final EditText txt = (EditText)dialog.findViewById(R.id.timeTotalQuiz);
+                  Button btn_dialog = (Button)dialog.findViewById(R.id.cancel);
+                  btn_dialog.setOnClickListener(new View.OnClickListener() {
+                      @Override
+                      public void onClick(View view) {
+                          dialog.cancel();
+                      }
+                  });
+                  Button btn_ok = (Button)dialog.findViewById(R.id.ok);
+                  btn_ok.setOnClickListener(new View.OnClickListener() {
+                      @Override
+                      public void onClick(View view) {
+
+                         Log.d("000000000000hhhhhhhhhh",nameQuiz.getText().toString()+"    "+password.getText().toString()
+                                  +"     "+idTeach+"   "+txt.getText().toString());
+                          if(!(txt.getText().toString()).isEmpty()){
+                              db.insertDataQuiz(nameQuiz.getText().toString(),Integer.parseInt(password.getText().toString()),idTeach
+                                      ,Integer.parseInt(txt.getText().toString()));
+                          Toast.makeText(getActivity(), "Add Quiz ", Toast.LENGTH_SHORT).show();
+                          dialog.cancel();
+                          nameQuiz.setText("");
+                          password.setText("");
+                          }else
+                              Toast.makeText(getActivity(), "Add total time  ", Toast.LENGTH_SHORT).show();
+                      }
+                  });
+
+                  dialog.show();
+
+              }
+            }
+            else
+                Toast.makeText(getActivity(), "Add nameQuiz and Password :(", Toast.LENGTH_SHORT).show();
+
+
+        }});
+
 
         return view;
     }
@@ -53,4 +171,5 @@ public class AddQuiz extends Fragment implements FragmentLifecycle{
     public void onResumeFragment() {
 
     }
+
 }
--- app/src/main/java/net/androidbootcamp/quiv_proj/TeacherStudentListview.java
+++ app/src/main/java/net/androidbootcamp/quiv_proj/TeacherStudentListview.java
@@ -1,15 +1,23 @@
 package net.androidbootcamp.quiv_proj;
 
 import android.app.Activity;
+import android.media.Image;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 
@@ -17,14 +25,16 @@ import java.util.ArrayList;
 public class TeacherStudentListview extends BaseAdapter {
    ArrayList<StudentItems> arrayList;
     Activity con;
-
+    DatabaseHelper db;
 
     ///
     TeacherStudentListview(Activity con,ArrayList<StudentItems>arrayList1) {
         this.con = con;
         arrayList=arrayList1;
-
+        db = new DatabaseHelper(con);
     }
+
+
     @Override
     public int getCount() {
 
@@ -42,7 +52,7 @@ public class TeacherStudentListview extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView( int i ,View view, ViewGroup viewGroup) {
         View row;
         final ViewHolder viewHolder;
         if (view == null) {
@@ -51,6 +61,15 @@ public class TeacherStudentListview extends BaseAdapter {
             viewHolder = new ViewHolder();
             viewHolder.name = (TextView) row.findViewById(R.id.name_student);
             viewHolder.id = (TextView) row.findViewById(R.id.id_student);
+            viewHolder.delete = (Button)row.findViewById(R.id.delStud);
+            viewHolder.delete.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    //Log.d("idddddd",viewHolder.id.getText().toString());
+                    db.deleteData(viewHolder.id.getText().toString());
+                    Toast.makeText(con, "delete Student ", Toast.LENGTH_SHORT).show();
+
+                                       }
+            });
 
             row.setTag(viewHolder);
 
@@ -60,7 +79,6 @@ public class TeacherStudentListview extends BaseAdapter {
         }
 
         final StudentItems item = arrayList.get(i);
-
         viewHolder.name.setText(item.name);
         viewHolder.id.setText(item.id+"");
         return row;
@@ -69,4 +87,5 @@ public class TeacherStudentListview extends BaseAdapter {
     public class ViewHolder {
         TextView name;
         TextView id;
+        Button delete;
     }}
